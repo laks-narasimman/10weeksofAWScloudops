a48d660570f259408107cb74e83d40f2dc080bae
laks <jainarasimma123@gmail.com>
2023-11-21 23:58:05 +0530

    correcting whole mess

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "week4-eksterra"
-    key    = "backend/ToDo-App.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "10weeksofcloudops-week4"
-  }
-}
