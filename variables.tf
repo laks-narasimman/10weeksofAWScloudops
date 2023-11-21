545d282d27277b0ec4f0a265bd5501fb9a717db3
laks <jainarasimma123@gmail.com>
2023-11-21 06:41:02 +0530

    pushing changes ti master

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable project_name{
+    description = "Name of your project"
+} 
+variable "region" {}
+#variable "project_name" {}
+variable "VPC_CIDR" {}
+variable "PUB_SUB_1_A_CIDR" {}
+variable "PUB_SUB_2_B_CIDR" {}
+variable "PRI_SUB_3_A_CIDR" {}
+variable "PRI_SUB_4_B_CIDR" {}
+variable "PRI_SUB_5_A_CIDR" {}
+variable "PRI_SUB_6_B_CIDR" {}
+variable "db_username" {}
+variable "db_password" {}
+variable "certificate_domain_name" {}
+variable "additional_domain_name" {}
+variable "my_ip" {}
+
